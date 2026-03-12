a138315fa1b4ace33e32b7a12e3b35b38ab5e6e8
DevTech <micahkesiena@gmail.com>
2026-03-12 13:05:11 +0100

    feat: improving

--- app/(admin)/admin/orders/page.tsx
+++ app/(admin)/admin/orders/page.tsx
@@ -2,26 +2,28 @@ import { Button } from "@/components/ui/button";
 
 export default function Orders() {
   return (
-    <main className="px-10">
-      <h1 className="text-4xl font-bold tracking-tight">Orders</h1>
+    <main className="px-4 md:px-10 py-6">
+      <div className="mb-8">
+        <h1 className="text-4xl font-bold tracking-tight uppercase">Orders</h1>
+      </div>
 
-      <div className="flex items-center justify-center gap-3">
-        <Button className="rounded-2xl" variant="outline">
+      <div className="flex flex-wrap items-center justify-start md:justify-center gap-3">
+        <Button className="rounded-2xl whitespace-nowrap" variant="outline">
           All
         </Button>
-        <Button className="rounded-2xl" variant="outline">
+        <Button className="rounded-2xl whitespace-nowrap" variant="outline">
           Paid
         </Button>
-        <Button className="rounded-2xl" variant="outline">
+        <Button className="rounded-2xl whitespace-nowrap" variant="outline">
           Pending
         </Button>
-        <Button className="rounded-2xl" variant="outline">
+        <Button className="rounded-2xl whitespace-nowrap" variant="outline">
           Shipped
         </Button>
-        <Button className="rounded-2xl" variant="outline">
+        <Button className="rounded-2xl whitespace-nowrap" variant="outline">
           Delivered
         </Button>
-        <Button className="rounded-2xl" variant="outline">
+        <Button className="rounded-2xl whitespace-nowrap" variant="outline">
           Cancelled
         </Button>
       </div>
